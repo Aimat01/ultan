797a3f1dd93549dd18ed6a62c456b66e3ae2f5c1
Aimat <aimat.kulmakhan@gmail.com>
2024-06-30 02:07:56 +0500

    version 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,36 @@
 <template>
-    <div class="container" :class = "background">
-        <header class="header">
-            <img :src="isDarkTheme ? menu_white.url : menu_black.url" class="menu" @click="toggleMenu">
-            <div class="logo">
-                <img :src="isDarkTheme ? light.url : dark.url" alt="Ultan Logo">
-            </div>
-            <div class="actions">
-                <button @click="toggleTheme" class="buttonTheme" :class="buttonColor">{{ isDarkTheme ? 'Light' : 'Dark' }}</button>
-                <select v-model="currentLanguage" @change="changeLanguage">
-                <option value="kk">Қазақ</option>
-                <option value="ru">Русский</option>
-                <option value="en">English</option>
-                </select>
-            </div>
-        </header>
+    <div class="container" :class="background" @click.self="closeMenu">
+      <header class="header" :class="borderBottom">
+        <img :src="isDarkTheme ? menu_white.url : menu_black.url" class="menu" @click.stop="toggleMenu">
+        <div class="logo">
+          <img :src="isDarkTheme ? light.url : dark.url" alt="Ultan Logo">
+        </div>
+        <div class="actions">
+          <button @click="toggleTheme" class="buttonTheme" :class="buttonColor">{{ isDarkTheme ? 'Light' : 'Dark' }}</button>
+          <select v-model="currentLanguage" @change="changeLanguage">
+            <option value="kk">Қазақ</option>
+            <option value="ru">Русский</option>
+            <option value="en">English</option>
+          </select>
+        </div>
+      </header>
+      <div class="menu-slider" :class="{ active: isMenuOpen }" @click.stop>
+        <ul>
+          <li><a href="#sales">Sales</a></li>
+          <li><a href="#bags">Bags</a></li>
+          <li><a href="#shoes">Shoes</a></li>
+        </ul>
+      </div>
     </div>
-</template>
+</template>  
+  
 
 <script>
 import Logo_dark from './assets/logo/dark.png';
 import Logo_light from './assets/logo/light.png';
 import Menu_black from './icons/menu-burger-black.svg';
 import Menu_white from './icons/menu-burger-white.svg';
+
 export default {
   data() {
     return {
@@ -29,8 +38,6 @@ export default {
       isChecked: false,
       isMenuOpen: false,
       currentLanguage: 'kk',
-      background: 'dark',
-      buttonColor: 'black',
       dark: {
         url: Logo_dark
       },
@@ -46,19 +53,25 @@ export default {
     };
   },
   computed: {
-    background(){
-      return this.isDarkTheme? 'dark' : 'light';
+    background() {
+      return this.isDarkTheme ? 'dark' : 'light';
     },
-    buttonColor(){
-        return this.isDarkTheme? 'white' : "black";
+    buttonColor() {
+      return this.isDarkTheme ? 'white' : 'black';
+    },
+    borderBottom() {
+      return this.isDarkTheme ? 'white_bottom' : 'black_bottom';
     }
   },
   methods: {
-    toggleTheme() { 
+    toggleTheme() {
       this.isDarkTheme = !this.isDarkTheme;
     },
-    toggleMenu(){
-      this.isMenuOpen = !this.isMenuOpen;  
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
     },
     changeLanguage(event) {
       this.$i18n.locale = event.target.value;
@@ -67,15 +80,17 @@ export default {
 };
 </script>
 
+
 <style scoped>
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 20px 20px;
+  /* position: sticky; */
 }
 
-.logo{
+.logo {
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
@@ -83,6 +98,16 @@ export default {
 
 .logo img {
   height: 40px;
+  cursor: pointer;
+}
+
+.nav ul {
+  display: flex;
+  list-style: none;
+}
+
+.nav li {
+  margin: 0 15px;
 }
 
 .actions {
@@ -95,72 +120,70 @@ export default {
   margin-left: 15px;
 }
 
-.buttonTheme{
-    border: none;
-    background: none;
+.buttonTheme {
+  border: none;
+  background: none;
+  cursor: pointer;
 }
 
-.black{
-    color: black;
+.black {
+  color: black;
 }
-.white{
-    color: white;
+.white {
+  color: white;
 }
 
-.menu{
-    height: 20px;
+.black_bottom {
+  border-bottom: 1px black solid;
+}
+.white_bottom {
+  border-bottom: 1px white solid;
+}
+
+.menu {
+  height: 16px;
+  cursor: pointer;
 }
 
 .menu-slider {
-  /* position: absolute;
-  width: 300px;
+  position: fixed;
+  top: 0;
+  left: -250px;
+  width: 250px;
   height: 100%;
-  background-color: #35231a;
-  padding: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transform: translateY(-100%);
-  transition: transform 0.3s ease;
+  background-color: #333;
+  color: white;
+  transition: left 0.3s ease;
+  padding-top: 60px;
   z-index: 1000;
-  display: none; */
 }
 
-.menu-slider ul li{
-    list-style: none;
-    color: white;
-    font-size: 20px;
-    padding: 20px 24px;
+.menu-slider.active {
+  left: 0;
 }
 
 .menu-slider ul {
-  list-style: none;
+  list-style-type: none;
   padding: 0;
-  margin: 0;
 }
 
-.menu-slider li {
-  margin-bottom: 10px;
+.menu-slider ul li {
+  padding: 10px;
+  text-align: center;
 }
 
-.menu-slider a {
-  color: #f5f0e0;
+.menu-slider ul li a {
+  color: white;
   text-decoration: none;
-  font-size: 14px;
 }
-
-.menu-slider-open {
-  transform: translateY(0);
-  display: block;
-}
-
 </style>
 
 <style>
-
 /* Theme */
-.dark{
+.dark {
   background-color: #35231a;
 }
-.light{
+.light {
   background-color: #f5f0e0;
 }
 </style>
